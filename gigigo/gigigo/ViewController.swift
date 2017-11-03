01c9beb4aa8b7bfa3a31432e7688cfa722723a71
Thiago Alexandre Araújo Santiago <thiago.santiago@easynvest.com.br>
2017-11-03 13:43:51 -0200

    Create the develop branch and the xcode project

---
+++ gigigo/gigigo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gigigo
+//
+//  Created by Thiago Alexandre Araújo Santiago on 03/11/2017.
+//  Copyright © 2017 Thiago Alexandre Araújo Santiago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
